bad2b363879cb0768b2cdacfbf8e549a73064709
Syed Muhammad Usman <s.m.usman10@gmail.com>
2022-10-27 21:01:13 +0500

    First commit

---
+++ LMS/Controllers/FeedController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.Controllers
+{
+    public class FeedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
