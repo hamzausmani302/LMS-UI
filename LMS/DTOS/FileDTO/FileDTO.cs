1ae5acafe4fa614fb832519005ad6356f9c61c35
hamzausmani302 <hamzausmani021@gmail.com>
2022-12-01 21:34:52 +0500

    test commit

--- LMS/DTOS/FileDTO/FileDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LMS.DTOS.FileDto
-{
-    public class FileDTO
-    {
-        public string? FileName { get; set; }
-        public byte[]? Data { get; set; }
-        public string? MimeType { get; set; }
-    }
-}
